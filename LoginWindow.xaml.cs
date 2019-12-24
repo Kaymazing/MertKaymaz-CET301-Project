d981bb4671893812b95eb1d46ae02fe238eacf16
Mert Kaymaz <mertfener@gmail.com>
2019-12-24 22:55:10 +0300

    Login Window added and its design partially completed.

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MertKaymaz_301Project
+{
+    /// <summary>
+    /// LoginWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
